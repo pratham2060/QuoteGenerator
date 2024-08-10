60d4af737280249f7b7881e12d7e4aedba3022a4
Prathamesh Charoskar <145451768+pratham2060@users.noreply.github.com>
2024-08-10 09:02:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const Quotes = require("randomquote-api");
-const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
@@ -32,35 +31,6 @@ app.get('/contact', (req, res) => {
   res.render('contact'); // This will render the contact.ejs file
 });
 
-// Create a transporter object using SMTP transport
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASS
-  }
-});
-
-// Handle contact form submission
-app.post('/submit-contact', (req, res) => {
-  const { name, email, message } = req.body;
-
-  const mailOptions = {
-      from: email,
-      to: process.env.GMAIL_USER,
-      subject: `Contact form submission from ${name}`,
-      text: `You have received a new message from ${name} (${email}):\n\n${message}`
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          console.error('Error sending email:', error);
-          return res.status(500).json({ success: false, error: error.toString() }); // More detailed error logging
-      }
-      console.log('Email sent:', info.response);
-      res.json({ success: true });
-  });
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
